d98121ddf3cae5974cfc1507d85df8fa396c08cd
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-06 11:27:24 -0300

    feat: create flow autheticantion

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+WebBrowser.maybeCompleteAuthSession();
+
+interface UserProps {
+  name: string;
+  avatarURL: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  isUserLoading: boolean;
+  signIn: () => Promise<void>;
+}
+
+interface AuthContextProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: "",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
+
+  async function signIn() {
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+
+  async function signInWithGoogle(accessToken: string) {}
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        user,
+        isUserLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
