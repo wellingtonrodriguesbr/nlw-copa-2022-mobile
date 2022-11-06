bbd985fe8549946e48f7953bae7978602d73bd02
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-06 09:49:32 -0300

    feat: create signIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,21 @@
-import { Center, Text } from "native-base";
+import { Center, Icon, Text } from "native-base";
+import { Button } from "../components/Button";
+import { Fontisto } from "@expo/vector-icons";
+import Logo from "../assets/logo.svg";
 
 export function SignIn() {
   return (
-    <Center flex={1} bg="gray.900">
-      <Text color="white" fontSize="xl">
-        SignIn
+    <Center flex={1} px={6} bg="gray.900">
+      <Logo width={212} height={40} />
+      <Button
+        mt={12}
+        title="Entrar com o Google"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
+      <Text mt={4} color="gray.200" textAlign="center" fontSize="sm">
+        Não utilizamos nenhuma informação além {"\n"}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   );
