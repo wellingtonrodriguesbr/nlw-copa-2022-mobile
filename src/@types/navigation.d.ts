6c87e309c7123c19df700db84474336fd911a0b0
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-11 06:54:45 -0300

    chore: add type to navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      new: undefined;
+      find: undefined;
+      pools: undefined;
+      details: {
+        id: string;
+      };
+    }
+  }
+}
