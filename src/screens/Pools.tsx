169abe678cfe3e06405dde88d07839f986557af7
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-12 11:06:02 -0300

    feat: listing pools

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,11 +1,37 @@
-import { Icon, VStack } from "native-base";
+import { FlatList, Icon, VStack } from "native-base";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { Octicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { useCallback, useEffect, useState } from "react";
+import { PoolCard, PoolProps } from "../components/PoolCard";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+import { api } from "../services/api";
+import { Loading } from "../components/Loading";
 
 export function Pools() {
   const { navigate } = useNavigation();
+  const [pools, setPools] = useState<PoolProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function getPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      getPools();
+    }, [])
+  );
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -25,6 +51,24 @@ export function Pools() {
           }
         />
       </VStack>
+      {!pools.length && isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          px={6}
+          data={pools}
+          keyExtractor={(pool) => pool.id}
+          renderItem={({ item }) => (
+            <PoolCard
+              data={item}
+              onPress={() => navigate("details", { id: item.id })}
+            />
+          )}
+          ListEmptyComponent={() => <EmptyPoolList />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+        />
+      )}
     </VStack>
   );
 }
